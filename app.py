870af6ef7a48317885f530da9f978c91f7e95f3b
andrei <andrei.zbikowski@getbraintree.com>
2014-11-25 21:45:26 -0600

    Added searching, normalize audio

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import os, sys
+
+
 from flask import Flask, request, g, jsonify, session
 from werkzeug import secure_filename
+from peewee import SQL
 
-from db import User, Song
+from db import MUSIC_DIR, User, Song, FTSSong, Playlist
 
 app = Flask("juicebox")
 app.secret_key = "swag"
 
-MUSIC_DIR = "/data/music"
 MUSIC_EXT = set(['mp3'])
 
 def allowed_file(filename):
@@ -18,10 +21,13 @@ def before_request():
     if 'id' in session:
         g.user = User.get(User.id == session['id'])
 
+    if "test" in request.headers:
+        g.user = User.get(User.id == request.headers.get("test"))
+
 @app.route("/upload", methods=["POST"])
 def route_upload():
     if not g.user:
-        return 401
+        return "", 401
 
     f = request.files["file"]
     if f and allowed_file(f.filename):
@@ -32,24 +38,27 @@ def route_upload():
 def route_api_songs():
     page = int(request.values.get("page", 1))
 
-    q = Song.select(Song.id, Song.title, Song.artist, User.username).join(User).paginate(page, 100)
+    q = Song.select(User.username,
+            *map(lambda i: getattr(Song, i), Song.DICT_FIELDS)).join(User).paginate(page, 100)
     return jsonify({
         "success": True,
         "page": page,
         "songs": map(lambda i: i.to_dict(), list(q))
     })
 
+@app.route("/api/playlists")
+def route_api_playlists():
+    page = int(request.values.get("page", 1))
+
 @app.route("/api/search")
 def route_api_search():
-    songs = Song.select(Song.title, Song.artist, Song.album).where(
-        (Song.title ** request.values.get("query")) |
-        (Song.album ** request.values.get("query")) |
-        (Song.artist ** request.values.get("query"))
-    ).limit(25)
+    songs = FTSSong.search(
+        request.values.get("query")
+    ).order_by(SQL('score')).limit(25)
 
     return jsonify({
         "success": True,
-        "songs": map(lambda i: i.to_dict(), list(songs))
+        "songs": map(lambda i: i.song.to_dict(), list(songs))
     })
 
 @app.route("/api/enqueue")
@@ -75,17 +84,17 @@ def route_login():
 
     if not u.check_password(pw):
         return jsonify({"success": False})
-    
+
     session["id"] = u.id
     return jsonify({"success": True})
 
 @app.route("/register")
 def route_register(x):
     params = {k:v for k, v in request.values.items() if k in ["username", "password", "email"]}
-    
+
     if not all(params.values()):
         return jsonify({"success": False, "msg": "Missing params"})
-    
+
     try:
         User.get((User.username == params["username"]) | (User.email == params["email"]))
         return jsonify({"success": False, "msg": "Username or Email already exists"})
@@ -93,7 +102,7 @@ def route_register(x):
 
     u = User(username=params["username"], email=params["email"])
     u.password = User.hash_password(params["password"])
-    
+
     session["id"] = u.save()
     return jsonify({"success": True})
 
@@ -101,5 +110,13 @@ def route_register(x):
 def route_stream():
     pass
 
-if __name__ == "__main__":
+def run():
+    if not os.path.exists(MUSIC_DIR):
+        print "Invalid MUSIC_DIR path `%s`!" % MUSIC_DIR
+        sys.exit(1)
+
     app.run("0.0.0.0", port=3000, debug=True)
+
+if __name__ == "__main__":
+    run()
+
--- db.py
+++ db.py
@@ -1,16 +1,66 @@
 import os, sys
 import bcrypt, eyed3, uuid
+
 from peewee import *
+from playhouse.sqlite_ext import SqliteExtDatabase, FTSModel
+
 from gravatar import Gravatar
 
-db = SqliteDatabase("juicebox.db")
+db = SqliteExtDatabase("juicebox.db", threadlocals=True)
 
-MUSIC_DIR = "./temp/"
+MUSIC_DIR = "data/music"
 MD5SUM = "md5" if sys.platform == "darwin" else "md5sum"
 
 class BModel(Model):
+    SEARCHABLE = False
+
     class Meta:
         database = db
+
+    def get_search_model(self):
+        return None
+
+    def save(self, *args, **kwargs):
+        id = Model.save(self, *args, **kwargs)
+
+        if self.SEARCHABLE:
+            search_model = self.get_search_model()
+            if bool(self._get_pk_value()):
+                search_model.update_from(self)
+            else:
+                search_model.create_from(self)
+        return id
+
+class SModel(FTSModel):
+    class Meta:
+        database = db
+
+    @classmethod
+    def update_fields(cls, instA, instB):
+        relation_name = instA.__class__.__name__.lower()
+        setattr(instB, relation_name, instA.id)
+
+        for field in cls._meta.fields.keys():
+            if field == "id" or field == relation_name: continue
+            setattr(instB, field, getattr(instA, field))
+
+    @classmethod
+    def create_from(cls, inst):
+        self = cls()
+        cls.update_fields(inst, self)
+        self.save()
+        return self
+
+    @classmethod
+    def update_from(cls, inst):
+        parent_name = inst.__class__.__name__.lower()
+        try:
+            self = cls.get(getattr(cls, parent_name) == inst)
+        except cls.DoesNotExist:
+            return cls.create_from(inst)
+        cls.update_fields(inst, self)
+        self.save()
+        return self
 
 class User(BModel):
     username = CharField()
@@ -31,6 +81,9 @@ class User(BModel):
         return bcrypt.hashpw(pw, self.password) == self.password
 
 class Song(BModel):
+    SEARCHABLE = True
+    DICT_FIELDS = ["id", "owner", "title", "artist", "album", "checksum"]
+
     class MediaType:
         SONG = 1
 
@@ -41,6 +94,9 @@ class Song(BModel):
     mediatype = IntegerField(default=MediaType.SONG)
     checksum = CharField(null=False)
     location = CharField()
+
+    def get_search_model(self):
+        return FTSSong
 
     def create_song_path(self):
         DIR = os.path.join(MUSIC_DIR, self.owner.username)
@@ -61,7 +117,8 @@ class Song(BModel):
 
     @classmethod
     def new_from_file(cls, user, fobj):
-        temp_name = os.path.join(MUSIC_DIR, str(uuid.uuid4()) + ".mp3")
+        temp_name_base = str(uuid.uuid4())
+        temp_name = os.path.join(MUSIC_DIR, temp_name_base + ".mp3")
         fobj.save(temp_name)
 
         # First lets grab the metadata
@@ -72,6 +129,11 @@ class Song(BModel):
 
         # Grab checksum
         checksum = os.popen("%s %s" % (MD5SUM, temp_name)).read().split(" ", 1)[0]
+
+        # Normalize Audio
+        new_temp_name = os.path.join(MUSIC_DIR, temp_name_base + "_normal.mp3")
+        os.popen("sox --norm %s %s" % (temp_name, new_temp_name))
+        temp_name = new_temp_name
 
         # We need some basic stuff
         if not meta.tag.artist or not meta.tag.title:
@@ -109,6 +171,15 @@ class Song(BModel):
     def open_meta(self):
         return eyed3.load(self.location)
 
+class FTSSong(SModel):
+    class Meta:
+        database = db
+
+    song = ForeignKeyField(Song)
+    title = TextField()
+    artist = TextField()
+    album = TextField()
+
 class Playlist(BModel):
     owner = ForeignKeyField(User)
     title = CharField()
@@ -123,7 +194,7 @@ class PlaylistEntry(BModel):
 # TODO: stats, likes
 
 if __name__ == "__main__":
-    for table in [User, Song, Playlist, PlaylistEntry]:
+    for table in [User, Song, Playlist, PlaylistEntry, FTSSong]:
         table.drop_table(True)
         table.create_table(True)
 
