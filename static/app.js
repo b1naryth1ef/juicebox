525f07c41bc0ff5de361b1cdf9ac0d50d79e0879
Andrew Hayworth <andrew.hayworth@getbraintree.com>
2015-01-09 15:53:20 -0600

    make slightly prettier
    - Use font-awesome to get nicer icons, make them bigger
    - Change the color for play/pause/stop icons
    - Better factor out the status-updating and global-variable-setting.
      This prevents us from re-rendering too quickly when changing the colors
      of the buttons.
    - Update status on page load so we get the actual current status of the MPD.
    - Nicer fonts. Slightly.

--- static/app.js
+++ static/app.js
@@ -53,19 +53,30 @@ $(function() {
         now_playing: new Song(Song.defaults)
       })
       this.mpd_template = _.template($('#mpd_template').html());
+      window.mpd = this.mpd;
+      this.update_status();
+    },
+    update_status: function() {
+      var _this = this;
+      $.getJSON('/api/player/status').success(function(data) {
+        _this.mpd.set({ now_playing: new Song($.extend(Song.defaults, data)) });
+        _this.render();
+      });
+
     },
     render: function() {
       var _this = this;
-      window.mpd = this.mpd;
+      var current_state = this.mpd.attributes.now_playing.attributes.state;
       $(this.el).html(this.mpd_template(_this.mpd.attributes.now_playing.attributes));
+      if (current_state == 'play') {
+        $('.fa-play').css('color', 'green');
+      } else if (current_state == 'stop' || current_state == 'pause') {
+        $('.fa-' + current_state).css('color', 'red');
+      }
       $(this.el).find('.control_link').on('click', function(e) {
         var operation = e.target.dataset.operation;
         _this.mpd[operation]();
-        $.getJSON('/api/player/status').success(function(data) {
-          _this.mpd.set({ now_playing: new Song($.extend(Song.defaults, data)) });
-          _this.render();
-        });
-        console.log(_this.mpd.attributes.now_playing.attributes);
+        _this.update_status();
       });
     }
   });
